538dcf2a9e9cb1739cd35b9a2aaa6ed33fc0982b
MintyImpetus <83627851+MintyImpetus@users.noreply.github.com>
2021-10-05 19:32:34 +0930

    Create inst.sh

---
+++ inst.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cp ~/nordrice/alacritty/alacritty.conf ~/.config/alacritty/alacritty.conf
+cp ~/nordrice/picom/picom.conf ~/.config/picom/picom.conf
+cp ~/nordrice/qtile/autostart.sh ~/.config/qtile/autostart.sh
+chmod +x ~/.config/qtile/autostart.sh
+cp ~/nordrice/rofi/config.rasi ~/.config/rofi/config.rasi
+cp ~/nordrice/btop/btop.conf ~/.config/btop/btop.conf
